5f598c5fb19206f435583d0d5101bad068461279
Bhavatharini Lakshmanan <bhavatharini.lakshmanan@gmail.com>
2025-02-03 12:00:34 +0530

    Day2

---
+++ wipro_java_project/src/wipro_java_project_oops/Employee.java
@@ -0,0 +1,47 @@
+package wipro_java_project_oops;
+
+public class Employee { 
+	private String empName;
+	private int empId;
+	private int age;
+	private String designation;
+	
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
