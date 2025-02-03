c137be6a46736ea2e6fa2ad6efed75b3524f5133
Bhavatharini Lakshmanan <bhavatharini.lakshmanan@gmail.com>
2025-02-03 12:34:44 +0530

    Day2

--- wipro_java_project/src/wipro_java_project_oops/Developer.java
+++ wipro_java_project/src/wipro_java_project_oops/Developer.java
@@ -20,6 +20,16 @@ public class Developer extends Employee {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Employee employee=new Developer();
+		employee.setEmpId(4567);
+		employee.setEmpName("Mohan");
+		employee.setAge(36);
+		employee.setDesignation("Developer");
+		
+		System.out.println(employee.getEmpId());
+		System.out.println(employee.getEmpName());
+		System.out.println(employee.getAge());
+	    System.out.println(employee.getDesignation());
 
 	}
 
