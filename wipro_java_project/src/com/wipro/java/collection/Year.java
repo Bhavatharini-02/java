3e6b9ea52400c065ee0b12c6c77e2355d261b44a
Bhavatharini Lakshmanan <bhavatharini.lakshmanan@gmail.com>
2025-02-06 11:41:16 +0530

    Day5

---
+++ wipro_java_project/src/com/wipro/java/collection/Year.java
@@ -0,0 +1,27 @@
+package com.wipro.java.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Year implements Comparator<Movie1> {
+	public int compare(Movie1 m1, Movie1 m2) {
+		return Integer.compare(m1.getY(), m2.getY());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Movie1> m1 = new ArrayList<>();
+		m1.add(new Movie1("Star Wars", 8.3, 2015));
+		m1.add(new Movie1("Empire Strikes Back", 8.7, 1977));
+		m1.add(new Movie1("Return of the Jedi", 8.8, 1980));
+		
+		Collections.sort(m1, new Year());
+		System.out.println("Movies sorted by year:");
+		for (Movie1 m : m1 ) {
+			System.out.println(m.getR() + " " + m.getN() + " " + m.getY());
+		}
+
+	}
+
+}
