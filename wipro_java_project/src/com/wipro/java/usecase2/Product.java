8815de32682bee4a4345ace3d2000cf85a803584
Bhavatharini Lakshmanan <bhavatharini.lakshmanan@gmail.com>
2025-02-10 19:41:10 +0530

    Day8

--- wipro_java_project/src/com/wipro/java/usecase2/Product.java
+++ wipro_java_project/src/com/wipro/java/usecase2/Product.java
@@ -1,43 +1,9 @@
 package com.wipro.java.usecase2;
 
-import java.time.LocalDate;
-
 public class Product {
-    private int id;
-    private String name;
-    private String category;
-    private double price;
-    private LocalDate manufactureDate;
 
-    // Constructor
-    public Product(int id, String name, String category, double price, LocalDate manufactureDate) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.price = price;
-        this.manufactureDate = manufactureDate;
-    }
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
 
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-
-    public LocalDate getManufactureDate() { return manufactureDate; }
-    public void setManufactureDate(LocalDate manufactureDate) { this.manufactureDate = manufactureDate; }
-
-    @Override
-    public String toString() {
-        return "Product { " + "ID=" + id + ", Name='" + name + '\'' +
-                ", Category='" + category + '\'' + ", Price=" + price +
-                ", ManufactureDate=" + manufactureDate + " }";
-    }
 }
--- wipro_java_project/src/com/wipro/java/usecase2/ProductManagement.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.java.usecase2;
-
-public class ProductManagement {
-
-	public ProductManagement() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
